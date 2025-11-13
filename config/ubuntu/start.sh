0cd1b1fcb78240095179b6436e2adebe3686b48a
John Coker <john@jcsw.com>
2025-11-13 07:50:21 -0800

    hide Chrome update dialog

--- config/ubuntu/start.sh
+++ config/ubuntu/start.sh
@@ -2,4 +2,4 @@
 xset s off
 xset s noblank
 unclutter -idle 1 &
-exec chromium --noerrdialogs --kiosk http://localhost/frame.html > /dev/null 2>&1
+exec chromium --noerrdialogs --kiosk --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' 'http://localhost/frame.html' > /dev/null 2>&1
